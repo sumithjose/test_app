ec129c5642c5a9885acf3f1ca37d878dc16b8f22
sumith jose <sumith@logiticks.com>
2021-04-13 20:00:35 +0000

    initial comit

---
+++ TestApplication/TestApplication.Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestApplication.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        //public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
